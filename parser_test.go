33fbdae1bff4d4a8a6fcd6cb57afbb6bef630f28
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2022-07-18 17:47:50 +0200

    init

---
+++ parser_test.go
@@ -0,0 +1,180 @@
+package INIParser
+
+import (
+	"reflect"
+	"testing"
+)
+
+var mapOfSections = map[SectionName]Section{
+	SectionName("owner") : {Key("name") : Value("John Doe"),
+							Key("organization") : Value("Acme Inc.")},
+
+	SectionName("database") : {Key("server") : Value("192.0.2.62"),
+							   Key("port") : Value("143"),
+							   Key("file") : Value("\"payroll.dat\""),},
+}
+
+var iniContent = `; last modified 1 April 2001 by John Doe
+[owner]
+name = John Doe
+organization = Acme Inc.
+
+[database]
+; use IP address in case network name resolution is not working
+server = 192.0.2.62     
+port = 143
+file = "payroll.dat"`
+
+var spacedIniContent = `; last modified 1 April 2001 by John Doe
+[owner]
+name = John Doe
+organization= Acme Inc.
+[database     ]
+; use IP address in case network name resolution is not working
+server      = 192.0.2.62     
+     port = 143
+file = "payroll.dat"`
+
+var emptyLinesIniContent = `; last modified 1 April 2001 by John Doe
+[owner]
+name = John Doe
+organization = Acme Inc.
+
+
+[database     ]
+; use IP address in case network name resolution is not working
+
+
+
+server = 192.0.2.62     
+port = 143
+
+;
+
+file = "payroll.dat"`
+
+func TestGetSections(t *testing.T) {
+	t.Run("get sections", func(t *testing.T) {
+		ini := IniFile{sections: mapOfSections}
+		got := ini.GetSections()
+		want := mapOfSections
+
+		assertEqualSections(t, got, want)
+	})
+
+	t.Run("nil sections", func(t *testing.T) {
+		ini := IniFile{}
+		got := ini.GetSections()
+
+		assertEqualSections(t, got, nil)
+	})
+}
+
+func TestGetSectionNames(t *testing.T) {
+	assertEqualLists := func(t testing.TB, got, want []SectionName) {
+		t.Helper()
+		if (!reflect.DeepEqual(got, want)) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("nil sections", func(t *testing.T) {
+		ini := IniFile{}
+		got := ini.GetSectionNames()
+		want := []SectionName{}
+		assertEqualLists(t, got, want)
+	})
+
+	t.Run("has sections", func(t *testing.T) {
+		ini := IniFile{sections: mapOfSections}
+		got := ini.GetSectionNames()
+		want := []SectionName{SectionName("owner"), SectionName("database")}
+
+		assertEqualLists(t, got, want)
+	})
+}
+
+func TestLoadFromFile(t *testing.T) {
+	assertEqualStrings := func(t testing.TB, got, want string) {
+		t.Helper()
+		if (!reflect.DeepEqual(got, want)) {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	t.Run("valid file path", func(t *testing.T) {
+		filePath := "/mnt/sda5/CS/codescallers/ahmed-salah-ucf-inigo/example.ini"
+		ini := IniFile{}
+
+		got, err := ini.LoadFromFile(filePath)
+		want := iniContent
+
+		if err != nil &&err.Error() == ErrInvalidFilePath.Error() {
+			t.Fatal("didn't expect to get an error.")
+		}
+
+		assertEqualStrings(t, got, want)
+	})
+
+	t.Run("invalid file path", func(t *testing.T) {
+		filepath := "/invalid/file/path/example.ini"
+		ini := IniFile{sections: map[SectionName]Section{}}
+		_, err := ini.LoadFromFile(filepath)
+
+		assertErrorMsg(t, err, ErrInvalidFilePath)
+	})
+
+	t.Run("invalid ini file syntax", func(t *testing.T) { // TO-Do
+		
+	})
+
+}
+
+func TestLoadFromString(t *testing.T) {
+	t.Run("nil sections", func(t *testing.T) {
+		data := iniContent
+
+		ini := IniFile{}
+		err := ini.LoadFromString(data)
+
+		assertErrorMsg(t, err, ErrNullReference)
+	})
+	t.Run("spaces trimming", func(t *testing.T) {
+		data := spacedIniContent
+
+		ini := IniFile{sections: map[SectionName]Section{}}
+		ini.LoadFromString(data)
+		got := ini.GetSections()
+		want := mapOfSections
+
+		assertEqualSections(t, got, want)
+	})
+
+	t.Run("empty lines", func(t *testing.T) {
+		data := emptyLinesIniContent
+
+		ini := IniFile{sections: map[SectionName]Section{}}
+		ini.LoadFromString(data)
+		got := ini.GetSections()
+		want := mapOfSections
+
+		assertEqualSections(t, got, want)
+	})
+}
+
+func assertEqualSections(t testing.TB, got, want map[SectionName]Section) {
+	t.Helper()
+	if (!reflect.DeepEqual(got, want)) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func assertErrorMsg(t testing.TB, err, want error) {
+	t.Helper()
+	if err == nil {
+		t.Fatal("expected to get an error.")
+	}
+
+	if err.Error() != want.Error() {
+		t.Errorf("got %s want %s", err.Error(), want)
+	}
+}
